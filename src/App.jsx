3592db8dbb77ac91a315388ca3c705126733d4d9
steve kumpevela <kumpevela@gmail.com>
2021-11-09 23:09:51 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css';
+import { Link, Route, BrowserRouter as Router } from "react-router-dom";
+import React from 'react';
+import DigitalClock from './components/DigitalClock/DigitalClock';
+import Stopwatch from './components/Stopwatch/Stopwatch';
+import Countdown from './components/Countdown/Countdown'
+
+const App = () => {
+  return ( 
+    
+    <Router>
+    <div className= "title"> My Watch </div>
+      <header>
+      <Link to="/">
+        <button>DigitalClock</button>
+        </Link>
+        <Link to="/Stopwatch">
+          <button>Stopwatch</button>
+      </Link>
+      <Link to="/Countdown">
+          <button>Countdown</button>
+      </Link>
+      </header>
+<div className= "hour">
+      <main>
+          <Route exact path="/" component={DigitalClock} />
+          <Route path="/Stopwatch" component={Stopwatch} />
+          <Route path="/Countdown" component={Countdown} />
+      </main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/DigitalClock/DigitalClock.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import "./DigitalClock.css"
+
+const DigitalClock = () => {
+  const [clockState, setClockState] = useState();
+
+  useEffect(() => {
+    setInterval(() => {
+      const date = new Date();
+      setClockState(date.toLocaleTimeString());
+    }, 1000);
+  }, []);
+  return (
+    <div className="DigitalClock">
+      <h2>{clockState}</h2>
+    </div>
+  );
+};
+
+export default DigitalClock;
